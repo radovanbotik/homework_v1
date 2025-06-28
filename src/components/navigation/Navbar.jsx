bf1e549c6232222984e0d68e02730a60f5c2aa55
radovanbotik <radovanbotik@gmail.com>
2025-06-28 11:39:01 +0200

    remove unnecessary div from navbar, move css up

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -4,16 +4,14 @@ import Container from "../ui/Container";
 
 export function Navbar() {
   return (
-    <Container className={"bg-[#233239] z-50"}>
-      <div className="flex w-full relative items-center h-12 text-white">
-        <Button
-          href={"/"}
-          className="static sm:absolute sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 bg-transparent text-sm hover:bg-inherit"
-        >
-          HALLO WORLD
-        </Button>
-        <ShoppingCart />
-      </div>
+    <Container className={"bg-[#233239] flex w-full relative items-center h-12 text-white"}>
+      <Button
+        href={"/"}
+        className="static sm:absolute sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 bg-transparent text-sm hover:bg-inherit"
+      >
+        HALLO WORLD
+      </Button>
+      <ShoppingCart />
     </Container>
   );
 }
