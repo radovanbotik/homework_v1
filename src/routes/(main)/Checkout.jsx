7fe4ac56b17dcb0ce253239bde7169d1058176e7
radovanbotik <radovanbotik@gmail.com>
2025-06-28 11:48:52 +0200

    remove unnecessary html and css from checkout page

--- src/routes/(main)/Checkout.jsx
+++ src/routes/(main)/Checkout.jsx
@@ -1,14 +1,13 @@
 import { Button } from "../../components/ui/Button";
+import { Header } from "../../components/ui/Header";
 
 export default function Checkout() {
   return (
-    <div className="w-full flex">
-      <div className="grid place-content-center mx-auto p-16">
-        <h2>You have reached the checkout page</h2>
-        <Button href={".."} relative="path">
-          Return to homepage
-        </Button>
-      </div>
-    </div>
+    <>
+      <Header title={"Welcome to our store"}></Header>
+      <Button href={"/listing-page"} relative="path" className="w-fit block mx-auto">
+        Return to products
+      </Button>
+    </>
   );
 }
